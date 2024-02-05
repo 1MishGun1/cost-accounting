374a3c4e5360c778dc2029046cefd6dba0713188
1MishGun1 <mikhail.kudrov@gmail.com>
2024-02-05 22:55:38 +0300

    update CostItem.js

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,23 +1,17 @@
 import "./CostItem.css";
+import CostDate from "./CostDate";
+import "./CostItem.css";
 
-function CostItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-
+const CostItem = (props) => {
   return (
     <div className="cost-item">
-      <div className="cost-date ">
-        <div className="cost-date__month">{month}</div>
-        <div className="cost-date__year">{year}</div>
-        <div className="cost-date__day">{day}</div>
-      </div>
+      <CostDate date={props.date} />
       <div className="cost-item__description">
         <h2>{props.description}</h2>
         <div className="cost-item__price">${props.costPrice}</div>
       </div>
     </div>
   );
-}
+};
 
 export default CostItem;
