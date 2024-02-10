ae89d45e8d06ba8eee8a2062337c6ecaf4032128
1MishGun1 <mikhail.kudrov@gmail.com>
2024-02-10 23:05:13 +0300

    update CostItem.js

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,15 +1,25 @@
 import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const CostItem = (props) => {
+  const [description, setDescription] = useState(props.description);
+
+  const showDiscriptionHandler = () => {
+    // description = "Test text";
+    setDescription("Test text"); // description = 'Test text' && CostItem(props)
+    console.log(description);
+  };
+
   return (
     <Card className="cost-item">
       <CostDate date={props.date} />
       <div className="cost-item__description">
-        <h2>{props.description}</h2>
+        <h2>{description}</h2>
         <div className="cost-item__price">${props.costPrice}</div>
       </div>
+      <button onClick={showDiscriptionHandler}>Edit direction</button>
     </Card>
   );
 };
