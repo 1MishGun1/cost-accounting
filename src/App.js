e4500818814f3bf8dd03f1a78fa97c4c9381b41b
1MishGun1 <mikhail.kudrov@gmail.com>
2024-02-17 23:03:39 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import NewCost from "./components/NewCost/NewCost";
 const App = () => {
   const costs = [
     {
+      id: "c1",
       date: new Date(2023, 2, 20),
       description: "Fridge",
       costPrice: 1000,
     },
     {
+      id: "c2",
       date: new Date(2023, 3, 15),
       description: "PC",
       costPrice: 1500,
     },
     {
+      id: "c3",
       date: new Date(2023, 4, 25),
       description: "Washing machine",
       costPrice: 1700,
@@ -29,10 +32,15 @@ const App = () => {
   //   React.createElement(Costs, { costs: costs })
   // );
 
+  const addCostHandler = (cost) => {
+    console.log(cost);
+    console.log("App Component");
+  };
+
   //? New version React code
   return (
     <div>
-      <NewCost />
+      <NewCost onAddCost={addCostHandler}/>
       <Costs costs={costs} />
     </div>
   );
