4397c94d44ba0dfc1614fab05966a11d571ebb50
1MishGun1 <mikhail.kudrov@gmail.com>
2024-02-19 22:28:01 +0300

    update Costs.js

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,26 +1,37 @@
 import "./Costs.css";
 import CostItem from "./CostItem";
 import Card from "../UI/Card";
+import CostsFilter from "../CostsFilter/CostsFilter";
+import { useState } from "react";
 
 const Costs = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2023");
+
+  const yearChangeHandler = (year) => {
+    setSelectedYear(year);
+  };
+
   return (
-    <Card className="costs">
-      <CostItem
-        date={props.costs[0].date}
-        description={props.costs[0].description}
-        costPrice={props.costs[0].costPrice}
-      />
-      <CostItem
-        date={props.costs[1].date}
-        description={props.costs[1].description}
-        costPrice={props.costs[1].costPrice}
-      />
-      <CostItem
-        date={props.costs[2].date}
-        description={props.costs[2].description}
-        costPrice={props.costs[2].costPrice}
-      />
-    </Card>
+    <>
+      <Card className="costs">
+        <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
+        <CostItem
+          date={props.costs[0].date}
+          description={props.costs[0].description}
+          costPrice={props.costs[0].costPrice}
+        />
+        <CostItem
+          date={props.costs[1].date}
+          description={props.costs[1].description}
+          costPrice={props.costs[1].costPrice}
+        />
+        <CostItem
+          date={props.costs[2].date}
+          description={props.costs[2].description}
+          costPrice={props.costs[2].costPrice}
+        />
+      </Card>
+    </>
   );
 };
 
