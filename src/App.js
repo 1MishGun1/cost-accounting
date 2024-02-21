3e441f4c19bf4c1ed18690a151458853136fbf60
1MishGun1 <mikhail.kudrov@gmail.com>
2024-02-21 22:44:02 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 // import React from "react";
 import Costs from "./components/Costs/Costs";
 import NewCost from "./components/NewCost/NewCost";
+import { useState } from "react";
+
+const INITIAL_COSTS = [
+  {
+    id: "c1",
+    date: new Date(2023, 2, 20),
+    description: "Fridge",
+    costPrice: 1000,
+  },
+  {
+    id: "c2",
+    date: new Date(2023, 3, 15),
+    description: "PC",
+    costPrice: 1500,
+  },
+  {
+    id: "c3",
+    date: new Date(2023, 4, 25),
+    description: "Washing machine",
+    costPrice: 1700,
+  },
+];
 
 const App = () => {
-  const costs = [
-    {
-      id: "c1",
-      date: new Date(2023, 2, 20),
-      description: "Fridge",
-      costPrice: 1000,
-    },
-    {
-      id: "c2",
-      date: new Date(2023, 3, 15),
-      description: "PC",
-      costPrice: 1500,
-    },
-    {
-      id: "c3",
-      date: new Date(2023, 4, 25),
-      description: "Washing machine",
-      costPrice: 1700,
-    },
-  ];
-
   //? Old version React code
   // return React.createElement(
   //   "div",
@@ -32,15 +33,18 @@ const App = () => {
   //   React.createElement(Costs, { costs: costs })
   // );
 
+  const [costs, setCosts] = useState(INITIAL_COSTS);
+
   const addCostHandler = (cost) => {
-    console.log(cost);
-    console.log("App Component");
+    setCosts((prevCosts) => {
+      return [cost, ...prevCosts];
+    });
   };
 
   //? New version React code
   return (
     <div>
-      <NewCost onAddCost={addCostHandler}/>
+      <NewCost onAddCost={addCostHandler} />
       <Costs costs={costs} />
     </div>
   );
