b39fd0b10ea25b0a2fde411d18d65937be45dcbb
1MishGun1 <mikhail.kudrov@gmail.com>
2024-02-26 11:12:35 +0300

    update CostItem.js (add delete btn)

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,8 +1,16 @@
 import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const CostItem = (props) => {
+  const [deleteCost, setDeleteCost] = useState(0);
+
+  const delCost = () => {
+    // deleteCost = "delete cost";
+    setDeleteCost("delete cost");
+    console.log(deleteCost);
+  };
 
   return (
     <Card className="cost-item">
@@ -10,6 +18,9 @@ const CostItem = (props) => {
       <div className="cost-item__description">
         <h2>{props.description}</h2>
         <div className="cost-item__price">${props.costPrice}</div>
+        <button className="delete-btn" onClick={delCost}>
+          Delete
+        </button>
       </div>
     </Card>
   );
