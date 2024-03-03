eebaa32762184143f2cf2e0f3913017ad88f34d3
1MishGun1 <mikhail.kudrov@gmail.com>
2024-03-03 22:42:56 +0300

    update Costs.js

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import CostsFilter from "../CostsFilter/CostsFilter";
 import { useState } from "react";
 import CostList from "./CostList";
+import CostsDiagram from "./CostsDiagram";
 
 const Costs = (props) => {
   const [selectedYear, setSelectedYear] = useState("2024");
@@ -11,7 +12,7 @@ const Costs = (props) => {
     setSelectedYear(year);
   };
 
-  const filterCost = props.costs.filter((cost) => {
+  const filteredCosts = props.costs.filter((cost) => {
     return cost.date.getFullYear().toString() === selectedYear;
   });
 
@@ -19,7 +20,8 @@ const Costs = (props) => {
     <>
       <Card className="costs">
         <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
-        <CostList costs={filterCost} />
+        <CostsDiagram costs={filteredCosts} />
+        <CostList costs={filteredCosts} />
       </Card>
     </>
   );
